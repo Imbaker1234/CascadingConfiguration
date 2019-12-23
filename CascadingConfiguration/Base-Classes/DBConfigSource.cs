e683929ba3601eff246b3beebc899c7f9c5bd5c9
Ian Baker <imbaker1234@gmail.com>
2019-12-23 02:30:30 -0500

    DBConfigSources now use SQLxt and the DbOperator class to retrieve their values.

--- CascadingConfiguration/Base-Classes/DBConfigSource.cs
+++ CascadingConfiguration/Base-Classes/DBConfigSource.cs
@@ -1,16 +1,17 @@
 ﻿using CascadingConfiguration;
+using SQLxt;
 
 namespace CascadingConfiguration.Classes.ConfigSource.Database
 {
     public abstract class DbConfigSource<T> : ConfigSource<T> where T : IConfig, new()
     { 
-        public Database Database { get; set; }
+        public IDbOperator DbOperator { get; set; }
         public string Table { get; set; }
         public string IdColumn { get; set; }
 
         protected DbConfigSource(string connectionString, string table, int priority) : base(priority)
         {
-            Database = new Database(connectionString);
+            DbOperator = new DbOperator(connectionString);
             Table = table;
         }
     }
